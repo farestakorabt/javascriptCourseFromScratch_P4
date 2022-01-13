e4598e146e3c722cd5e0453ebe20e1b3bcfd0c4d
fares takorabt <farestakorabt@gmail.com>
2022-01-13 09:50:05 +0100

    implement meal-appli

--- cours/index.js
+++ cours/index.js
@@ -80,3 +80,64 @@ async function fetchData() {
 
   executerLaFonction();
 }
+
+// --------------------------
+// JSON
+// --------------------------
+
+// methode .json() => methode qui s'auto-resout en renvoyant le body de la requete.
+fetch("data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    // console.log("data:", data);
+
+    let seetings2 = JSON.stringify(data); // stringify => convertit en JSON
+    // console.log("seetings2:", seetings2);
+
+    let parsed2 = JSON.parse(seetings2); // Parse => transforme json en js
+    // console.log("parsed2:", parsed2);
+  });
+
+// --------------------------
+// web API
+// --------------------------
+
+// CLIENT STORAGE
+// --------------------------
+
+// Local Storage
+
+// local storage =  10mo peuvent PAS etre récupérés par le site
+// cookies = 4ko => peuvent etre récupérés par le site
+
+localStorage.data = "je stocke la data";
+// console.log(localStorage.data);
+// localStorage.removeItem("data");
+// localStorage.clear();
+// document.body.textContent = localStorage.data;
+
+const objet = {
+  name: "Roberto",
+  age: 112,
+};
+
+// il faut passer des chaines de caracteres
+localStorage.user = JSON.stringify(objet);
+
+// console.log(JSON.parse(localStorage.user)); // =>
+// age: 112
+// name: "Roberto"
+
+// --------------------
+// session storage => active juisqu'à l'extinction du pc
+// sessionStorage.dataSettings3 = "56 meters";
+// console.log(sessionStorage.dataSettings3);
+// sessionStorage.clear();
+
+// --------------------
+// COOKIES
+
+document.cookie = "nickname=darius"; // value => key, nickname => darius
+
+// Bonne pratique
+document.cookie = "pseudo=Pat;path=/,max-age=45000,secure;samesite";
